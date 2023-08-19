8ff5f4ea2fdeef5e45315840d23ca36674bdc279
Felipe1501 <felipebsantos1501@gmail.com>
2023-08-19 19:20:21 -0300

    anatomy of classes

--- src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("HELLO HELLO FIIII!");
-        System.out.println("Primeiro Programa em Java!!");
-    }
-}
