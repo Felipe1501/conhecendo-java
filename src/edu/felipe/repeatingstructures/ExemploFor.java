04bb91cf1a85b41c37d12e174a17cae27d4866e5
Felipe1501 <felipebsantos1501@gmail.com>
2023-08-23 12:25:51 -0300

    foreach

--- src/edu/felipe/repeatingstructures/ExemploFor.java
+++ src/edu/felipe/repeatingstructures/ExemploFor.java
@@ -5,5 +5,16 @@ public class ExemploFor {
         for( int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
             System.out.println("Contando Carneirinhos " + carneirinhos);
         }
+
+        String alunos[] = {"FELIPE", "YURI ALBERTO", "MATIAS ROJAS"};
+
+        for(int x=0; x<alunos.length; x++){
+            System.out.println("O Aluno no índice x= " + x + " é " + alunos[x]);
+        }
+
+        //forma abreviada
+        for(String aluno : alunos){
+            System.out.println(aluno);
+        }
     }
 }
