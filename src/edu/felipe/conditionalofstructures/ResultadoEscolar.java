09f51657582f055c05e3847c093c1824abb5fa16
Felipe1501 <felipebsantos1501@gmail.com>
2023-08-23 11:50:57 -0300

    conditional pt.3

--- src/edu/felipe/conditionalofstructures/ResultadoEscolar.java
+++ src/edu/felipe/conditionalofstructures/ResultadoEscolar.java
@@ -2,10 +2,12 @@ package edu.felipe.conditionalofstructures;
 
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 10;
 
         if(nota >= 7)
             System.out.println("Aprovado");
+        else if(nota >= 5 && nota < 7)
+            System.out.println("Prova Recuperação");
         else
             System.out.println("Reprovado");
     }
