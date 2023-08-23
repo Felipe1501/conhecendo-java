3d22f7c935eaba40188906ec2144270d9841f3a0
Felipe1501 <felipebsantos1501@gmail.com>
2023-08-23 12:01:28 -0300

    conditional pt.4

--- src/edu/felipe/conditionalofstructures/ResultadoEscolar.java
+++ src/edu/felipe/conditionalofstructures/ResultadoEscolar.java
@@ -10,5 +10,9 @@ public class ResultadoEscolar {
             System.out.println("Prova Recuperação");
         else
             System.out.println("Reprovado");
+
+
+        String resultado = nota >= 7 ?  "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
     }
 }
